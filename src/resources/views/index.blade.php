8b3544a438e5409547fa7ea00ee13fbf367d39ce
shimotaroo <shimotaro.0327@gmail.com>
2021-03-19 13:46:11 +0900

    Laravel-mixでscssをコンパイルする設定追加、Google Fonts使用設定

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,17 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
 
+    <!-- CSS -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
-    <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
-
-    <!-- Styles -->
-    {{-- <link rel="stylesheet" href="https://hypertext-candy.s3-ap-northeast-1.amazonaws.com/posts/vue-laravel-tutorial/app.css"> --}}
-    </head>
-    <body>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&display=swap" rel="stylesheet">
+</head>
+<body>
     <div id="app"></div>
 </body>
 </html>
