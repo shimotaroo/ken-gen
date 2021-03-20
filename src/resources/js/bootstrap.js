c134a2a1755116fd51729ea063b7868dcca05430
shimotaroo <shimotaro.0327@gmail.com>
2021-03-21 00:08:09 +0900

    コメント追加

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -8,6 +8,7 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+// Ajaxリクエストであることを示すヘッダーを付与する
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
