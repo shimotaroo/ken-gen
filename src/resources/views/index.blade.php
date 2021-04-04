99ee86fb37f239d25f52039c80c898894c505906
shimotaroo <shimotaro.0327@gmail.com>
2021-04-04 15:00:28 +0900

    OGP設定

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,15 +4,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- OGP --}}
+    <meta property="og:title" content="{{ config('app.name') }}">
+    <meta property="og:image" content="{{ asset('/image/summary-image.jpg') }}">
+    <meta property="og:description" content="{{ config('app.description') }}">
+    <meta property="og:url" content="{{ config('app.url') }}">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="{{ config('app.name') }}">
+    <meta name="twitter:site" content="{{ config('app.url') }}">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="{{ config('app.name') }}">
+    <meta name="twitter:image" content="{{ asset('/image/summary-image.jpg') }}">
+    <meta name="twitter:description" content="{{ config('app.description') }}">
     <title>{{ config('app.name') }}</title>
-
-    <!-- CSS -->
+    {{-- CSS --}}
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
-    <!-- Scripts -->
+    {{-- Scripts --}}
     <script src="{{ mix('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
+    {{-- favicon --}}
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+    {{-- Fonts --}}
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
